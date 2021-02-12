ea70fd148c1360ab1589eb9e57498f7cdb4e54ae
André <andrevictor50@gmail.com>
2021-02-11 23:50:04 -0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3db89c83ea97c2e68787b2078d732560'),
+    },
+  },
+});
