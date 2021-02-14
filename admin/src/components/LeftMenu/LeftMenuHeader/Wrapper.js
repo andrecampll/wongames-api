ab956d39cd60e93b894a792c2801dd4c8fbeec8a
André <andrevictor50@gmail.com>
2021-02-14 14:57:45 -0300

    Adding themes and code refactor on styled components

--- admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
+++ admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
@@ -1,50 +1,33 @@
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
 
 import Logo from '../../../assets/images/logo-won.svg';
 
 const Wrapper = styled.div`
-  background-color: #18202e;
-  padding-left: 2rem;
-  height: ${props => props.theme.main.sizes.leftMenu.height};
+${({ theme }) => css`
+    background-color: ${theme.main.colors.won.blue};
+    padding-left: 2rem;
+    height: ${theme.main.sizes.leftMenu.height};
 
-  .leftMenuHeaderLink {
-    &:hover {
-      text-decoration: none;
+    .leftMenuHeaderLink {
+      &:hover {
+        text-decoration: none;
+      }
     }
-  }
 
-  .projectName {
-    display: block;
-    width: 100%;
-    height: ${props => props.theme.main.sizes.leftMenu.height};
-    font-size: 2rem;
-    letter-spacing: 0.2rem;
-    color: $white;
+    .projectName {
+      display: block;
+      width: 100%;
+      height: ${theme.main.sizes.leftMenu.height};
+      font-size: 2rem;
+      letter-spacing: 0.2rem;
+      color: $white;
 
-    background-image: url(${Logo});
-    background-repeat: no-repeat;
-    background-position: left center;
-    background-size: auto 2.5rem;
-  }
+      background-image: url(${Logo});
+      background-repeat: no-repeat;
+      background-position: left center;
+      background-size: auto 2.5rem;
+    }
+  `}
 `;
-
-Wrapper.defaultProps = {
-  theme: {
-    main: {
-      colors: {
-        leftMenu: {},
-      },
-      sizes: {
-        header: {},
-        leftMenu: {},
-      },
-    },
-  },
-};
-
-Wrapper.propTypes = {
-  theme: PropTypes.object,
-};
 
 export default Wrapper;
